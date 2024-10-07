9bce3f5c169da2cb283f2a1464fd8ffb7ecd5737
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-10-07 13:26:29 +0200

    Konfigurerer opp databasetilkobling.

--- src/test/kotlin/no/nav/ung/deltakelseopplyser/TestUngDeltakelseOpplyserApplication.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.ung.deltakelseopplyser
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-	fromApplication<UngDeltakelseOpplyserApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
--- src/test/kotlin/no/nav/ung/deltakelseopplyser/UngDeltakelseOpplyserApplicationTests.kt
+++ src/test/kotlin/no/nav/ung/deltakelseopplyser/UngDeltakelseOpplyserApplicationTests.kt
@@ -2,14 +2,13 @@ package no.nav.ung.deltakelseopplyser
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
 
-@Import(TestcontainersConfiguration::class)
+@ActiveProfiles("test")
 @SpringBootTest
 class UngDeltakelseOpplyserApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
