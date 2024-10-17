d7c2dd49f137c108488993767a2e5221e776fa87
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-10-17 11:05:51 +0200

    Dropper og oppretter

--- src/main/resources/db/migration/V1__initial.sql
+++ src/main/resources/db/migration/V1__initial.sql
@@ -1,11 +1,2 @@
 DROP TABLE IF EXISTS ungdomsprogram;
 DROP TABLE IF EXISTS ungdomsprogram_deltakelse;
-/*CREATE TABLE ungdomsprogram_deltakelse
-(
-    id                  UUID PRIMARY KEY NOT NULL,
-    deltaker_ident      VARCHAR(20)      NOT NULL,
-    fra_og_med          DATE             NOT NULL,
-    til_og_med          DATE,
-    opprettet_tidspunkt TIMESTAMP        NOT NULL,
-    endret_tidspunkt    TIMESTAMP
-);*/
---
+++ src/main/resources/db/migration/V2__initial.sql
@@ -0,0 +1,9 @@
+CREATE TABLE ungdomsprogram_deltakelse
+(
+    id                  UUID PRIMARY KEY NOT NULL,
+    deltaker_ident      VARCHAR(20)      NOT NULL,
+    fra_og_med          DATE             NOT NULL,
+    til_og_med          DATE,
+    opprettet_tidspunkt TIMESTAMP        NOT NULL,
+    endret_tidspunkt    TIMESTAMP
+);
