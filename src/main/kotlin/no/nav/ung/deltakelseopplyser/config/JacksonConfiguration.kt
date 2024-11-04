f17992ae0e8d6a9880e4423e35ebe16369139fab
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-11-04 15:05:22 +0100

    Customizer jackson med JavaTimeModule.

--- src/main/kotlin/no/nav/ung/deltakelseopplyser/config/JacksonConfiguration.kt
+++ src/main/kotlin/no/nav/ung/deltakelseopplyser/config/JacksonConfiguration.kt
@@ -1,16 +1,16 @@
 package no.nav.ung.deltakelseopplyser.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 @Configuration
 class JacksonConfiguration {
 
     @Bean
-    fun kotlinModule(): KotlinModule = KotlinModule.Builder().build()
-
-    @Bean
-    fun javaTimeModule(): JavaTimeModule = JavaTimeModule()
+    fun jackson2ObjectMapperBuilderCustomizer(builder: Jackson2ObjectMapperBuilder) = builder
+        .modules(JavaTimeModule())
+        .build<ObjectMapper>()
 }
