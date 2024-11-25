94a45a9845ad75067bcb97937c85fe4b1163d058
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-11-25 15:31:44 +0100

    Glemte contraint på referanse til deltaker.

--- src/main/resources/db/migration/V1__initial.sql
+++ src/main/resources/db/migration/V1__initial.sql
@@ -1,4 +1,13 @@
+DROP TABLE IF EXISTS ungdomsprogram_deltakelse;
+DROP TABLE IF EXISTS deltaker;
+
 CREATE EXTENSION IF NOT EXISTS btree_gist;
+
+CREATE TABLE IF NOT EXISTS deltaker
+(
+    id             UUID PRIMARY KEY NOT NULL,
+    deltaker_ident VARCHAR(20) NOT NULL UNIQUE
+);
 
 CREATE TABLE IF NOT EXISTS ungdomsprogram_deltakelse
 (
@@ -10,11 +19,9 @@ CREATE TABLE IF NOT EXISTS ungdomsprogram_deltakelse
     endret_tidspunkt    TIMESTAMP,
 
     -- Sjekker at det ikke er overlappende perioder for samme deltaker
-    CONSTRAINT ingen_overlappende_periode EXCLUDE USING GIST (deltaker_id WITH =, periode WITH &&)
-);
+    CONSTRAINT ingen_overlappende_periode EXCLUDE USING GIST (deltaker_id WITH =, periode WITH &&),
 
-CREATE TABLE IF NOT EXISTS deltaker
-(
-    id             UUID PRIMARY KEY NOT NULL,
-    deltaker_ident VARCHAR(20) NOT NULL UNIQUE
+    -- Referanser
+    -- Sjekker at deltaker_id refererer til en eksisterende deltaker
+    CONSTRAINT fk_deltaker_id FOREIGN KEY (deltaker_id) REFERENCES deltaker(id)
 );
