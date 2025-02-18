7214f52850aa377c4605a723db06263cb35f0cc6
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-02-18 14:14:31 +0100

    søknad skal ikke kunne være null.

--- src/main/resources/db/migration/V2__ung_soknad.sql
+++ src/main/resources/db/migration/V2__ung_soknad.sql
@@ -1,8 +1,8 @@
 CREATE TABLE ung_søknad
 (
-    journalpost_id          VARCHAR(50) PRIMARY KEY NOT NULL,
-    søker_ident             VARCHAR(20)             NOT NULL,
-    søknad                  jsonb,
-    opprettet_dato          TIMESTAMP               NOT NULL,
-    oppdatert_dato          TIMESTAMP               NOT NULL
+    journalpost_id VARCHAR(50) PRIMARY KEY NOT NULL,
+    søker_ident    VARCHAR(20)             NOT NULL,
+    søknad         jsonb                   NOT NUll,
+    opprettet_dato TIMESTAMP               NOT NULL,
+    oppdatert_dato TIMESTAMP               NOT NULL
 );
