7431068ae8abd1d3f35daa041d5a2be391fd9bac
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-02-26 11:27:51 +0100

    Formaterer datoer som string.

--- src/main/kotlin/no/nav/ung/deltakelseopplyser/oppgave/OppgavetypeDataDAO.kt
+++ src/main/kotlin/no/nav/ung/deltakelseopplyser/oppgave/OppgavetypeDataDAO.kt
@@ -1,5 +1,6 @@
 package no.nav.ung.deltakelseopplyser.oppgave
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDate
 
@@ -7,13 +8,13 @@ import java.time.LocalDate
 sealed class OppgavetypeDataDAO
 
 data class EndretStartdatoOppgavetypeDataDAO(
-    val nyStartdato: LocalDate,
+    @JsonFormat(pattern = "yyyy-MM-dd") val nyStartdato: LocalDate,
     @JsonProperty(defaultValue = "n/a") val veilederRef: String = "n/a",
     val meldingFraVeileder: String?,
 ) : OppgavetypeDataDAO()
 
 data class EndretSluttdatoOppgavetypeDataDAO(
-    val nySluttdato: LocalDate,
+    @JsonFormat(pattern = "yyyy-MM-dd")val nySluttdato: LocalDate,
     @JsonProperty(defaultValue = "n/a") val veilederRef: String,
     val meldingFraVeileder: String?,
 ) : OppgavetypeDataDAO()
