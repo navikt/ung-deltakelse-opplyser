3bc75d94ce1849672d717b2630124f19bdbc394c
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-03-06 14:27:32 +0100

    Fikser filter

--- src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/oppgave/kafka/UngdomsytelseOppgavebekreftelseKonsument.kt
+++ src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/oppgave/kafka/UngdomsytelseOppgavebekreftelseKonsument.kt
@@ -3,7 +3,7 @@ package no.nav.ung.deltakelseopplyser.domene.oppgave.kafka
 import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.ung.deltakelseopplyser.config.TxConfiguration.Companion.TRANSACTION_MANAGER
 import no.nav.ung.deltakelseopplyser.domene.oppgave.OppgaveService
-import no.nav.ung.deltakelseopplyser.domene.soknad.kafka.UngdomsytelsesøknadKonsumentConfiguration.Companion.UNGDOMSYTELSESØKNAD_FILTER
+import no.nav.ung.deltakelseopplyser.domene.oppgave.kafka.UngdomsytelseOppgavebekreftelseKonsumentConfiguration.Companion.UNGDOMSYTELSESOPPGAVEBEKREFTELSE_FILTER
 import no.nav.ung.deltakelseopplyser.utils.Constants
 import no.nav.ung.deltakelseopplyser.utils.MDCUtil
 import org.slf4j.LoggerFactory
@@ -31,7 +31,7 @@ class UngdomsytelseOppgavebekreftelseKonsument(
         id = "#{'\${topic.listener.ung-oppgavebekreftelse.id}'}",
         groupId = "#{'\${spring.kafka.consumer.group-id}'}",
         autoStartup = "#{'\${topic.listener.ung-oppgavebekreftelse.bryter}'}",
-        filter = UNGDOMSYTELSESØKNAD_FILTER,
+        filter = UNGDOMSYTELSESOPPGAVEBEKREFTELSE_FILTER,
         properties = [
             "auto.offset.reset=#{'\${topic.listener.ung-oppgavebekreftelse.auto-offset-reset}'}"
         ]
@@ -40,7 +40,10 @@ class UngdomsytelseOppgavebekreftelseKonsument(
         @Payload ungdomsytelseOppgavebekreftelseTopicEntry: String,
     ) {
         val oppgavebekreftelseTopicEntry =
-            objectMapper.readValue(ungdomsytelseOppgavebekreftelseTopicEntry, UngdomsytelseOppgavebekreftelseTopicEntry::class.java)
+            objectMapper.readValue(
+                ungdomsytelseOppgavebekreftelseTopicEntry,
+                UngdomsytelseOppgavebekreftelseTopicEntry::class.java
+            )
         logger.info("Mottar og håndterer UngdomsytelseOppgavebekreftelse for ungdomsytelsen")
         oppgaveService.håndterMottattOppgavebekreftelse(oppgavebekreftelseTopicEntry.data.journalførtMelding)
         logger.info("Håndtert UngdomsytelseOppgavebekreftelse for ungdomsytelsen.")
@@ -53,13 +56,14 @@ class UngdomsytelseOppgavebekreftelseKonsumentConfiguration(
 ) {
     companion object {
         private val logger = LoggerFactory.getLogger(UngdomsytelseOppgavebekreftelseKonsumentConfiguration::class.java)
-        const val UNGDOMSYTELSESOPPGAVEBEKREFTELSE_FILTER = "ungdomsytelsesOppgavebekreftelseFilter"
+        internal const val UNGDOMSYTELSESOPPGAVEBEKREFTELSE_FILTER = "ungdomsytelsesOppgavebekreftelseFilter"
     }
 
     @Bean(UNGDOMSYTELSESOPPGAVEBEKREFTELSE_FILTER)
     fun ungdomsytelseOppgavebekreftelseFilter() = RecordFilterStrategy<String, String> { consumerRecord ->
         try {
-            val oppgavebekreftelseTopicEntry = objectMapper.readValue(consumerRecord.value(), UngdomsytelseOppgavebekreftelseTopicEntry::class.java)
+            val oppgavebekreftelseTopicEntry =
+                objectMapper.readValue(consumerRecord.value(), UngdomsytelseOppgavebekreftelseTopicEntry::class.java)
             MDCUtil.toMDC(Constants.CORRELATION_ID, oppgavebekreftelseTopicEntry.metadata.correlationId)
             MDCUtil.toMDC(Constants.JOURNALPOST_ID, oppgavebekreftelseTopicEntry.data.journalførtMelding.journalpostId)
             logger.info("Deserialisert UngdomsytelseOppgavebekreftelse fra topic")
