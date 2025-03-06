e2ad08ef6b69f8e43a922d6b81b8b6f6cd9c355a
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-03-06 14:46:00 +0100

    Legger på logging.

--- src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/oppgave/OppgaveService.kt
+++ src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/oppgave/OppgaveService.kt
@@ -2,6 +2,7 @@ package no.nav.ung.deltakelseopplyser.domene.oppgave
 
 import no.nav.ung.deltakelseopplyser.domene.oppgave.kafka.UngdomsytelseOppgavebekreftelse
 import no.nav.ung.deltakelseopplyser.domene.register.UngdomsprogramDeltakelseRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -9,14 +10,19 @@ import java.util.UUID
 class OppgaveService(
     private val deltakelseRepository: UngdomsprogramDeltakelseRepository
 ) {
+    private companion object {
+        private val logger = LoggerFactory.getLogger(OppgaveService::class.java)
+    }
 
     fun håndterMottattOppgavebekreftelse(ungdomsytelseOppgavebekreftelse: UngdomsytelseOppgavebekreftelse) {
         val oppgaveBekreftelse = ungdomsytelseOppgavebekreftelse.oppgaveBekreftelse
         val oppgaveId = UUID.fromString(oppgaveBekreftelse.søknadId.id)
 
+        logger.info("Henter deltakelse for oppgaveId=$oppgaveId")
         val deltakelse = deltakelseRepository.finnDeltakelseGittOppgaveId(oppgaveId) ?: throw RuntimeException("Fant ikke deltakelse for oppgaveId=$oppgaveId")
         val oppgave = deltakelse.oppgaver.find { it.id == oppgaveId } ?: throw RuntimeException("Fant ikke oppgave for oppgaveId=$oppgaveId")
 
+        logger.info("Markerer oppgave som løst for deltakelseId=${deltakelse.id}")
         val løstOppgave = oppgave.markerSomLøst()
         deltakelse.oppdaterOppgave(løstOppgave)
 
