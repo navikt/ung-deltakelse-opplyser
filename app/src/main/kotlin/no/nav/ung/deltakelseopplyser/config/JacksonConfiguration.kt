596d10db51a75584d26b9b0123331558bceca530
Espen Velsvik <espen.velsvik@nav.no>
2025-03-23 12:35:30 +0100

    Splitter ut kontraktkode i egen modul

--- app/src/main/kotlin/no/nav/ung/deltakelseopplyser/config/JacksonConfiguration.kt
+++ app/src/main/kotlin/no/nav/ung/deltakelseopplyser/config/JacksonConfiguration.kt
@@ -3,7 +3,7 @@ package no.nav.ung.deltakelseopplyser.config
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxJacksonConfigurationml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
