9b7c695d25c58752c53c6aad567ab87e0e6ee698
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-03-28 13:22:01 +0100

    SummerInntekt skal være nullable.

--- kontrakt/src/main/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/deltaker/RapportPeriodeinfoDTO.kt
+++ kontrakt/src/main/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/deltaker/RapportPeriodeinfoDTO.kt
@@ -9,5 +9,5 @@ data class RapportPeriodeinfoDTO(
     val harRapportert: Boolean,
     val arbeidstakerOgFrilansInntekt: BigDecimal? = null,
     val inntektFraYtelse: BigDecimal? = null,
-    val summertInntekt: BigDecimal = arbeidstakerOgFrilansInntekt?.add(inntektFraYtelse ?: BigDecimal.ZERO) ?: BigDecimal.ZERO,
+    val ≈: BigDecimal? = arbeidstakerOgFrilansInntekt?.add(inntektFraYtelse ?: BigDecimal.ZERO) ?: null,
 )
