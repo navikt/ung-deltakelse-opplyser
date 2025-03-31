bdb7464a54e993457e5dacbe90b587d1ef4f0c16
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-03-31 08:18:48 +0200

    Oppretter indeks hvis det ikke eksisterer.

--- app/src/main/resources/db/migration/V6__ung_rapportert_inntekt.sql
+++ app/src/main/resources/db/migration/V6__ung_rapportert_inntekt.sql
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS ung_rapportert_inntekt
 
 
 -- Indeks på 'søker_ident' som optimaliserer spørringer på kolonnen 'søker_ident'.
-CREATE INDEX idx_ung_rapportert_inntekt_soker_ident ON ung_rapportert_inntekt (søker_ident);
+CREATE INDEX IF NOT EXISTS idx_ung_rapportert_inntekt_soker_ident ON ung_rapportert_inntekt (søker_ident);
 
 -- GIN-indeks på 'inntekt' for JSONB-operasjoner.
 -- Den forbedrer ytelsen på spørringer som benytter søk etter bestemte nøkkel/verdi-par i JSON-strukturen.
-CREATE INDEX idx_ung_rapportert_inntekt_inntekt ON ung_rapportert_inntekt USING gin (inntekt);
+CREATE INDEX IF NOT EXISTS idx_ung_rapportert_inntekt_inntekt ON ung_rapportert_inntekt USING gin (inntekt);
