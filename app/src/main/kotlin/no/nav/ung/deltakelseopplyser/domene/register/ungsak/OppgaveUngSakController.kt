5de85487edb85805c4bf8adf362498bd9adc1483
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-04-02 09:51:00 +0200

    Returnerer oppgaven som er opprettet.

--- app/src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/register/ungsak/OppgaveUngSakController.kt
+++ app/src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/register/ungsak/OppgaveUngSakController.kt
@@ -7,22 +7,27 @@ import no.nav.security.token.support.core.api.RequiredIssuers
 import no.nav.ung.deltakelseopplyser.config.Issuers
 import no.nav.ung.deltakelseopplyser.domene.deltaker.DeltakerDAO
 import no.nav.ung.deltakelseopplyser.domene.deltaker.DeltakerService
-import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.felles.KontrollerRegisterinntektOppgavetypeDataDTO
-import no.nav.ung.deltakelseopplyser.domene.oppgave.repository.*
+import no.nav.ung.deltakelseopplyser.domene.oppgave.repository.ArbeidOgFrilansRegisterInntektDAO
+import no.nav.ung.deltakelseopplyser.domene.oppgave.repository.KontrollerRegisterInntektOppgaveTypeDataDAO
+import no.nav.ung.deltakelseopplyser.domene.oppgave.repository.OppgaveDAO
 import no.nav.ung.deltakelseopplyser.domene.oppgave.repository.OppgaveDAO.Companion.tilDTO
-import no.nav.ung.deltakelseopplyser.kontrakt.register.DeltakelseOpplysningDTO
-import no.nav.ung.deltakelseopplyser.domene.register.UngdomsprogramDeltakelseDAO
+import no.nav.ung.deltakelseopplyser.domene.oppgave.repository.RegisterinntektDAO
+import no.nav.ung.deltakelseopplyser.domene.oppgave.repository.YtelseRegisterInntektDAO
 import no.nav.ung.deltakelseopplyser.domene.register.UngdomsprogramDeltakelseRepository
-import no.nav.ung.deltakelseopplyser.domene.register.UngdomsprogramregisterService.Companion.mapToDTO
+import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.felles.KontrollerRegisterinntektOppgavetypeDataDTO
+import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.felles.OppgaveDTO
 import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.felles.OppgaveStatus
 import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.felles.Oppgavetype
 import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.registerinntekt.RegisterInntektOppgaveDTO
-
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ProblemDetail
 import org.springframework.web.ErrorResponseException
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import java.util.*
@@ -64,7 +69,7 @@ class OppgaveUngSakController(
     @PostMapping("/opprett", produces = [MediaType.APPLICATION_JSON_VALUE])
     @Operation(summary = "Oppretter oppgave")
     @ResponseStatus(HttpStatus.OK)
-    fun opprettOppgaveForKontrollAvRegisterinntekt(@RequestBody opprettOppgaveDto: RegisterInntektOppgaveDTO): DeltakelseOpplysningDTO {
+    fun opprettOppgaveForKontrollAvRegisterinntekt(@RequestBody opprettOppgaveDto: RegisterInntektOppgaveDTO): OppgaveDTO {
         val deltaker = deltakerService.finnDeltakerGittIdent(opprettOppgaveDto.aktørId) ?: throw ErrorResponseException(
             HttpStatus.NOT_FOUND,
             ProblemDetail.forStatus(HttpStatus.NOT_FOUND).also {
@@ -72,9 +77,10 @@ class OppgaveUngSakController(
             },
             null
         )
-        val deltakersOppgaver = deltakerService.hentDeltakersOppgaver(opprettOppgaveDto.aktørId)
 
-        val deltakelse = forsikreEksistererIProgram(deltaker)
+        forsikreEksistererIProgram(deltaker)
+
+        val deltakersOppgaver = deltakerService.hentDeltakersOppgaver(opprettOppgaveDto.aktørId)
 
         val harUløstOppgaveForSammePeriode = deltakersOppgaver.stream()
             .anyMatch {
@@ -117,7 +123,7 @@ class OppgaveUngSakController(
 
         deltaker.leggTilOppgave(nyOppgave)
         deltakerService.oppdaterDeltaker(deltaker)
-        return deltakelse.mapToDTO()
+        return nyOppgave.tilDTO()
     }
 
     private fun gjelderSammePeriode(it: OppgaveDAO, ny: RegisterInntektOppgaveDTO): Boolean {
@@ -126,7 +132,7 @@ class OppgaveUngSakController(
         return !eksisterende.fraOgMed.isAfter(ny.tomDato) && !eksisterende.tilOgMed.isBefore(ny.fomDato)
     }
 
-    private fun forsikreEksistererIProgram(deltaker: DeltakerDAO): UngdomsprogramDeltakelseDAO {
+    private fun forsikreEksistererIProgram(deltaker: DeltakerDAO) {
         val deltakterIder = deltakerService.hentDeltakterIder(deltaker.deltakerIdent)
         val deltagelser = deltakelseRepository.findByDeltaker_IdIn(deltakterIder)
 
@@ -139,17 +145,5 @@ class OppgaveUngSakController(
                 null
             )
         }
-
-        if (deltagelser.size > 1) {
-            throw ErrorResponseException(
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR).also {
-                    it.detail = "Fant flere deltakelser med id ${deltaker.id}"
-                },
-                null
-            )
-        }
-        return deltagelser.first()
     }
-
 }
