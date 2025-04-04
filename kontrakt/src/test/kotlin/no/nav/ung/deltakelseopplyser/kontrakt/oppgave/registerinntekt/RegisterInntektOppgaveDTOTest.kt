bc158b79b6c0a9058270fd72894f309c45323f50
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-04-04 13:07:05 +0200

    Revert navnendring

--- kontrakt/src/test/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/oppgave/registerinntekt/RegisterInntektOppgaveDTOTest.kt
+++ kontrakt/src/test/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/oppgave/registerinntekt/RegisterInntektOppgaveDTOTest.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class EndretPeriodeOppgaveDTOTest {
+class RegisterInntektOppgaveDTOTest {
 
 
     internal val mapper = ObjectMapper().registerModule(JavaTimeModule())
