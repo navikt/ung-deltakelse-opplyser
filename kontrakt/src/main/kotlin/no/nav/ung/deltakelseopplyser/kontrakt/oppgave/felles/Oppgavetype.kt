727edea9a36d2758b77471abe0e13245c72c7a58
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-04-22 12:24:06 +0200

    Oppretter oppgave-varsler på mine-sider (#49)
    * Publiserer oppgaver på mine-sider.
    
    * Kjører med transaksjon
    
    * Deaktiverer i transaksjon.
    
    * Flytter til app modul.
    
    * Oppretter og deaktiverer varsler i sync med oppgaver.
    
    * Fikser url.
    
    * Sletter controller brukt for testing.
    
    * Adresserer PR-kommentarer.

--- kontrakt/src/main/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/oppgave/felles/Oppgavetype.kt
+++ kontrakt/src/main/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/oppgave/felles/Oppgavetype.kt
@@ -1,6 +1,6 @@
 package no.nav.ung.deltakelseopplyser.kontrakt.oppgave.felles
 
-enum class Oppgavetype {
-    BEKREFT_ENDRET_PROGRAMPERIODE,
-    BEKREFT_AVVIK_REGISTERINNTEKT,
+enum class Oppgavetype(val mineSiderVarselTekst: String) {
+    BEKREFT_ENDRET_PROGRAMPERIODE("Du har fått en oppgave om å bekrefte endret programperiode"),
+    BEKREFT_AVVIK_REGISTERINNTEKT("Du har fått en oppgave om å bekrefte inntekten din"),
 }
