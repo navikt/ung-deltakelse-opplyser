aec1a0b02e9a8267536e79eefbb286fc1eba451e
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-05-12 09:56:10 +0200

    Definerer ytelseType som enum med kodeverdi. (#85)
    * Definerer ytelseType som enum med kodeverdi.
    
    * Smartere test.
    
    * Fjerner kode fra YtelseType

--- kontrakt/src/test/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/oppgave/registerinntekt/RegisterInntektOppgaveDTOTest.kt
+++ kontrakt/src/test/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/oppgave/registerinntekt/RegisterInntektOppgaveDTOTest.kt
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 
 class RegisterInntektOppgaveDTOTest {
 
@@ -44,6 +46,36 @@ class RegisterInntektOppgaveDTOTest {
         assertEquals(1, dto.registerInntekter.registerinntekterForArbeidOgFrilans?.size)
         assertEquals(10000, dto.registerInntekter.registerinntekterForArbeidOgFrilans?.get(0)?.beløp)
         assertEquals("910909088", dto.registerInntekter.registerinntekterForArbeidOgFrilans?.get(0)?.arbeidsgiverIdent)
+    }
+
+    @ParameterizedTest
+    @EnumSource(YtelseType::class)
+    fun `skal serialisere input med ytelse`(ytelseType: YtelseType) {
+        //language=JSON
+        val utenYtelse = """
+            {
+              "deltakerIdent": "99154987302",
+              "referanse": "0fc3f6fc-0eb1-4c51-9822-61ab3e71090b",
+              "frist": "2025-04-04T07:37:20.251902",
+              "fomDato": "2025-01-01",
+              "tomDato": "2025-02-01",
+              "registerInntekter": {
+                "registerinntekterForYtelse": [
+                  {
+                    "beløp": 10000,
+                    "ytelseType": "${ytelseType.name}"
+                  }
+              ]
+            }
+            }"""
+        val dto: RegisterInntektOppgaveDTO = mapper.readValue(utenYtelse, RegisterInntektOppgaveDTO::class.java)
+
+        assertEquals("99154987302", dto.deltakerIdent)
+        assertEquals(1, dto.registerInntekter.registerinntekterForYtelse?.size)
+
+        val inntektYtelseDTO = dto.registerInntekter.registerinntekterForYtelse?.first()
+        assertEquals(10000, inntektYtelseDTO?.beløp)
+        assertEquals(ytelseType, inntektYtelseDTO?.ytelseType)
     }
 
 
