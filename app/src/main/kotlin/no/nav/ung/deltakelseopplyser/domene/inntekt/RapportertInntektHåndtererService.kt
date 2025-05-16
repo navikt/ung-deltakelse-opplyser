c007ce853c5c91f7d674597d9d10ca87bf73e251
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-05-16 08:38:44 +0200

    Løser oppgave om rapportert inntekt.

--- app/src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/inntekt/RapportertInntektHåndtererService.kt
+++ app/src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/inntekt/RapportertInntektHåndtererService.kt
@@ -6,13 +6,17 @@ import no.nav.ung.deltakelseopplyser.domene.deltaker.DeltakerService
 import no.nav.ung.deltakelseopplyser.domene.inntekt.kafka.UngdomsytelseRapportertInntekt
 import no.nav.ung.deltakelseopplyser.domene.inntekt.repository.RapportertInntektRepository
 import no.nav.ung.deltakelseopplyser.domene.inntekt.repository.UngRapportertInntektDAO
+import no.nav.ung.deltakelseopplyser.domene.varsler.MineSiderVarselService
+import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.felles.Oppgavetype
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class RapportertInntektHåndtererService(
     private val rapportertInntektRepository: RapportertInntektRepository,
-    private val deltakerService: DeltakerService
+    private val deltakerService: DeltakerService,
+    private val mineSiderVarselService: MineSiderVarselService
 ) {
     private companion object {
         private val logger = LoggerFactory.getLogger(RapportertInntektHåndtererService::class.java)
@@ -22,12 +26,21 @@ class RapportertInntektHåndtererService(
         logger.info("Håndterer mottatt rapportert inntekt.")
         val rapportertInntekt: Søknad = rapportertInntektTopicEntry.rapportertInntekt
         val deltakerIdent = rapportertInntekt.søker.personIdent.verdi
+        val oppgaveReferanse = UUID.fromString(rapportertInntekt.søknadId.id)
 
         logger.info("Henter deltakerIder for søker oppgitt i rapportert inntekt")
-        val deltakterIder = deltakerService.hentDeltakterIder(deltakerIdent)
-        if (deltakterIder.isEmpty()) {
-            throw IllegalStateException("Fant ingen deltakere med ident oppgitt i rapportert inntekt")
-        }
+        val deltaker = deltakerService.finnDeltakerGittIdent(deltakerIdent)
+            ?: throw IllegalStateException("Fant ingen deltakere med ident oppgitt i rapportert inntekt")
+
+        val oppgave = deltakerService.hentDeltakersOppgaver(deltakerIdent)
+            .find { it.oppgaveReferanse == oppgaveReferanse && it.oppgavetype == Oppgavetype.RAPPORTER_INNTEKT }
+            ?: throw RuntimeException("Deltaker har ikke oppgave for oppgaveReferanse=$oppgaveReferanse")
+
+        logger.info("Markerer oppgave som løst for deltaker=${deltaker.id}")
+        oppgave.markerSomLøst()
+
+        logger.info("Deaktiverer oppgave med oppgaveReferanse=$oppgaveReferanse da den er løst")
+        mineSiderVarselService.deaktiverOppgave(oppgave.oppgaveReferanse.toString())
 
         logger.info("Lagrer rapportert inntekt med journalpostId: {}", rapportertInntektTopicEntry.journalpostId)
         rapportertInntektRepository.save(rapportertInntektTopicEntry.somRapportertInntektDAO())
