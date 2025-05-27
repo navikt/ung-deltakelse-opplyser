d4b9e07a9c7fd33e5b14f4ad4eb5ca93e8f7ef36
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-05-27 12:47:00 +0200

    Fjerner Transactional

--- app/src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/minside/mikrofrontend/MikrofrontendService.kt
+++ app/src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/minside/mikrofrontend/MikrofrontendService.kt
@@ -1,10 +1,8 @@
 package no.nav.ung.deltakelseopplyser.domene.minside.mikrofrontend
 
 import no.nav.tms.microfrontend.Sensitivitet
-import no.nav.ung.deltakelseopplyser.config.TxConfiguration
 import no.nav.ung.deltakelseopplyser.domene.minside.MineSiderService
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
 class MikrofrontendService(
@@ -12,7 +10,6 @@ class MikrofrontendService(
     private val mineSiderService: MineSiderService,
 ) {
 
-    @Transactional(transactionManager = TxConfiguration.TRANSACTION_MANAGER, rollbackFor = [Exception::class])
     fun sendOgLagre(mikrofrontendDAO: MikrofrontendDAO) {
         mineSiderService.aktiverMikrofrontend(
             deltakerIdent = mikrofrontendDAO.deltaker.deltakerIdent,
