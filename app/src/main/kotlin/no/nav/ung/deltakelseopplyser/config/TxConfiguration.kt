f0b7733056e811da868be095b3aa566f3c7b9614
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-05-27 13:28:21 +0200

    Fikser transaksjonshåndtering.

--- app/src/main/kotlin/no/nav/ung/deltakelseopplyser/config/TxConfiguration.kt
+++ app/src/main/kotlin/no/nav/ung/deltakelseopplyser/config/TxConfiguration.kt
@@ -3,18 +3,18 @@ package no.nav.ung.deltakelseopplyser.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
-import org.springframework.orm.jpa.JpaTransactionManager
-import jakarta.persistence.EntityManagerFactory
+import org.springframework.jdbc.datasource.DataSourceTransactionManager
+import javax.sql.DataSource
 
 @Configuration
 class TxConfiguration {
     companion object {
-        const val TRANSACTION_MANAGER = "transactionManager"
+        const val TRANSACTION_MANAGER = "dstm"
     }
 
     @Bean(TRANSACTION_MANAGER)
     @Primary
-    fun transactionManager(entityManagerFactory: EntityManagerFactory): JpaTransactionManager {
-        return JpaTransactionManager(entityManagerFactory)
+    fun dstm(dataSource: DataSource): DataSourceTransactionManager {
+        return DataSourceTransactionManager(dataSource)
     }
 }
