c93d406c89d3195018fab1baa88c1e457e1eb836
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-08-11 08:03:23 +0200

    Fikser BigQueryKonfig.kt
    Bruker no credentials for vtp.

--- app/src/main/kotlin/no/nav/ung/deltakelseopplyser/statistikk/bigquery/BigQueryKonfig.kt
+++ app/src/main/kotlin/no/nav/ung/deltakelseopplyser/statistikk/bigquery/BigQueryKonfig.kt
@@ -1,5 +1,6 @@
 package no.nav.ung.deltakelseopplyser.statistikk.bigquery
 
+import com.google.cloud.NoCredentials
 import com.google.cloud.bigquery.BigQuery
 import com.google.cloud.bigquery.BigQueryOptions
 import org.springframework.context.annotation.Bean
@@ -11,9 +12,18 @@ import org.springframework.context.annotation.Profile
 class BigQueryKonfig {
 
     @Bean(name = ["bigQuery"])
-    fun bigQuery(): BigQuery {
+    @Profile(value = ["prod-gcp", "dev-gcp"])
+    fun bigQueryDefaultCredentials(): BigQuery {
         return BigQueryOptions.getDefaultInstance().service
     }
 
+    @Bean(name = ["bigQuery"])
+    @Profile(value = ["vtp"])
+    fun bigQueryNoCredentials(): BigQuery {
+        return BigQueryOptions.newBuilder()
+            .setProjectId("test-project")
+            .setCredentials(NoCredentials.getInstance())
+            .build()
+            .service
+    }
 }
-
