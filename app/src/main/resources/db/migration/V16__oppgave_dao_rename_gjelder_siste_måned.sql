22619d34357927f366b6347c6cad415aef341016
Espen Velsvik <espen.velsvik@nav.no>
2025-12-01 12:08:04 +0100

    Fikse sql

--- app/src/main/resources/db/migration/V16__oppgave_dao_rename_gjelder_siste_måned.sql
+++ app/src/main/resources/db/migration/V16__oppgave_dao_rename_gjelder_siste_måned.sql
@@ -1,8 +1,8 @@
 UPDATE oppgave
-SET oppgavetype_data =jsonb_set(
+SET oppgavetype_data = jsonb_set(
         oppgavetype_data - 'gjelderSisteMåned', -- Fjerner gammel felt
         '{gjelderDelerAvMåned}', -- Setter nytt felt
         to_jsonb(coalesce(oppgavetype_data ->> 'gjelderSisteMåned', 'false')), -- kopierer verdien
         true -- Oppretter felt hvis det ikke finnes og
-                      ) from oppgave
+                      )
 WHERE oppgavetype_data IS NOT NULL and oppgavetype in ('RAPPORTER_INNTEKT', 'BEKREFT_AVVIK_REGISTERINNTEKT');
