f481d475a6e04550a04e60d66e5072095d592179
Espen Velsvik <espen.velsvik@nav.no>
2026-01-21 15:04:47 +0100

    TSFF-2308: Fjerner har_opphoersvedtak fra historikktabell

--- app/src/main/resources/db/migration/V18__deltakelse_har_opphoersvedtak.sql
+++ app/src/main/resources/db/migration/V18__deltakelse_har_opphoersvedtak.sql
@@ -1,5 +1,2 @@
 ALTER TABLE IF EXISTS ungdomsprogram_deltakelse
     ADD COLUMN har_opphoersvedtak BOOLEAN DEFAULT FALSE NOT NULL;
-
-ALTER TABLE IF EXISTS ungdomsprogram_deltakelse_historikk
-    ADD COLUMN har_opphoersvedtak BOOLEAN DEFAULT FALSE NOT NULL;
