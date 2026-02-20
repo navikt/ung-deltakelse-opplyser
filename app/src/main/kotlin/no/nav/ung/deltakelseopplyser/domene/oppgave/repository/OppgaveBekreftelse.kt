93a5952e235b1346f6b10b792538ac2603cb81d6
Espen Velsvik <espen.velsvik@nav.no>
2026-02-20 15:18:46 +0100

    FIX: OppgaveBekreftelse implementerer Serializable

--- app/src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/oppgave/repository/OppgaveBekreftelse.kt
+++ app/src/main/kotlin/no/nav/ung/deltakelseopplyser/domene/oppgave/repository/OppgaveBekreftelse.kt
@@ -1,6 +1,8 @@
 package no.nav.ung.deltakelseopplyser.domene.oppgave.repository
 
+import java.io.Serializable
+
 class OppgaveBekreftelse(
     val harUttalelse: Boolean,
     val uttalelseFraBruker: String? = null,
-)
+) : Serializable
