d5cd33581f4d7da89fabc56cc30397fc85fb6816
Espen Velsvik <espen.velsvik@nav.no>
2026-03-13 08:49:46 +0100

    PeriodeDto implementerer serializable for å kunne parses

--- kontrakt/src/main/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/oppgave/felles/PeriodeDTO.kt
+++ kontrakt/src/main/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/oppgave/felles/PeriodeDTO.kt
@@ -2,6 +2,7 @@ package no.nav.ung.deltakelseopplyser.kontrakt.oppgave.felles
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.io.Serializable
 import java.time.LocalDate
 
 data class PeriodeDTO (
@@ -10,4 +11,4 @@ data class PeriodeDTO (
 
     @JsonFormat(pattern = "yyyy-MM-dd")
     @JsonProperty("tom")val tom: LocalDate?
-)
+) : Serializable
