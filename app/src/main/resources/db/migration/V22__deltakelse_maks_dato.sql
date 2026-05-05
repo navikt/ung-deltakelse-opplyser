da39562e4da01d0ce54d1febac6e89063ed3060f
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2026-05-05 15:00:41 +0200

    Fjerner V22 migrasjon - bruker KvotePeriodeBeregner istedenfor kolonne

--- app/src/main/resources/db/migration/V22__deltakelse_maks_dato.sql
+++
@@ -1,5 +0,0 @@
--- Legger til maks_dato på deltakelse-tabellen.
--- Brukes til å schedulere automatisk opphør når maksdato er nådd.
-alter table ungdomsprogram_deltakelse add column maks_dato date;
-
-comment on column ungdomsprogram_deltakelse.maks_dato is 'Beregnet maksdato for deltakelsen (260/300 virkedager fra startdato). Settes av veileder ved innmelding.';
