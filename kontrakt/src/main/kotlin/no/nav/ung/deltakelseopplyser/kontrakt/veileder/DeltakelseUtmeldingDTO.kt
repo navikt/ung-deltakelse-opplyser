c5bfd68babd13ea09afedd08ac5adff8e13e9a0e
Ramin Esfandiari <ramin.esfandiari@nav.no>
2026-08-14 09:13:35 +0200

    Legg til nullable avslutningsårsak i DeltakelseUtmeldingDTO for bakoverkompatibilitet

--- kontrakt/src/main/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/veileder/DeltakelseUtmeldingDTO.kt
+++ kontrakt/src/main/kotlin/no/nav/ung/deltakelseopplyser/kontrakt/veileder/DeltakelseUtmeldingDTO.kt
@@ -6,6 +6,7 @@ import java.time.LocalDate
 
 data class DeltakelseUtmeldingDTO(
     @JsonProperty("utmeldingsdato") val utmeldingsdato: LocalDate,
+    // TODO: Fjern nullable når dette er lansert og frontend har migrert. Den er nullable for bakoverkompatibilitet.
     @JsonProperty("avslutningsårsak") val avslutningsårsak: Avslutningsårsak? = null,
 ) {
 
